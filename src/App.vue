bf07a983b356175dc801b4b579fa80b9deabcdbf
= <=>
2020-09-10 20:21:03 +0800

    準備素材

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="12">
+          <div class="choosePhoto logo"></div>
+        </b-col>
+      </b-row>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </b-container>
   </div>
 </template>
 
@@ -28,5 +34,16 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.choosePhoto {
+  height: 20rem;
+}
+
+.logo {
+  background-image: url("./assets/cover.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: center;
 }
 </style>
