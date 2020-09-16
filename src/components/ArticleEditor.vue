2b4855884d8d3f59e64e6f6fbed53c2326760c67
= <=>
2020-09-16 23:10:48 +0800

    editor

--- src/components/ArticleEditor.vue
+++ src/components/ArticleEditor.vue
@@ -2,6 +2,10 @@
   <b-container class="pageArticleEditor">
     <b-row>
       <b-col cols="12">
+        <label for="input-large">文章標題:</label>
+        <b-form-input id="input-large" size="lg" placeholder="請輸入文章標題" v-model="title"></b-form-input>
+      </b-col>
+      <b-col cols="12">
         <MarkdownPro
           @on-save="updateArticle"
         ></MarkdownPro>
@@ -13,10 +17,12 @@
 
 <script>
 import { MarkdownPro } from 'vue-meditor'
+import { firebase } from '../Model/FirebaseModel.js'
 export default {
   name: 'ArticleEditor',
   data () {
     return {
+      title: ''
     }
   },
   components: {
@@ -24,16 +30,25 @@ export default {
   },
   methods: {
     updateArticle (content) {
-      console.log(content)
+      const update = content
+      update.title = this.title
+      console.log(update)
     }
   }
 }
 </script>
 
-<style language="scss" scope>
+<style lang="scss" scope>
 .pageArticleEditor {
   padding: 1rem;
+
+  & > .row {
+    & > div {
+      margin: 1rem;;
+    }
+  }
 }
+
 .markdown-body {
   border: #aaa 2px solid;
   border-radius: 5px;
